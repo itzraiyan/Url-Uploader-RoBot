a282f8e2a8842c68ffaf78bcb30d5b854c2680a3
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-19 16:00:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 
 class Config(object):
     # Bot Information 
-    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "7211200915:AAGbeMt_Nox-3JqUOI7tQxswwrfL4AwQ-nM")
+    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "7211200915:AAHYmpFqwjyhvVqdXZzjQuBBn7HaJna1gfs")
     TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "URL_upload_RoBot") # Bot username without @.
     
     # The Telegram API things
